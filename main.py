bd68b875a043c26e03887e51c50154762ab87791
AbdelrahmanElmughrabi <abdrahman.mughrbai14@gmail.com>
2025-04-24 22:28:53 +0300

    Added explination

--- main.py
+++ main.py
@@ -4,44 +4,43 @@ from paddle import Paddle
 from ball import Ball
 from scoreboard import Scoreboard
 
-
-
+# Screen setup
 screen=Screen()
 screen.listen()
 screen.setup(width=800,height=600)
 screen.bgcolor("black")
 screen.title("Pong")
-screen.tracer(0)
+screen.tracer(0)  # Turn off animation updates
 
-paddle_r=Paddle(350)
-paddle_l=Paddle(-350)
+# Create game objects
+paddle_r=Paddle(350)  # Right paddle
+paddle_l=Paddle(-350)  # Left paddle
 ball=Ball()
 scoreboard=Scoreboard()
 
-
-screen.onkey(fun=paddle_r.up, key="Up")
+# Set up keyboard controls
+screen.onkey(fun=paddle_r.up, key="Up")     # Right paddle controls
 screen.onkey(fun=paddle_r.down, key="Down")
-screen.onkey(fun=paddle_l.up, key="w")
+screen.onkey(fun=paddle_l.up, key="w")      # Left paddle controls
 screen.onkey(fun=paddle_l.down, key="s")
 
 game_is_on=True
 
-
-
-
+# Main game loop
 while game_is_on:
     time.sleep(ball.move_speed)
-    screen.update()
+    screen.update()  # Update screen manually
     ball.move()
 
+    # Detect collision with top and bottom walls
     if ball.ycor()>280 or ball.ycor()<-280:
         ball.bounce_y()
 
-    # detect collision with paddle
+    # Detect collision with paddles
     if ball.distance(paddle_r)<50 and ball.xcor()>320 or ball.distance(paddle_l)<50 and ball.xcor()<-320:
         ball.bounce_x()
 
-
+    # Score points when ball passes paddles
     if ball.xcor()>380:
         print("l scored on right panel")
         scoreboard.l_point()
@@ -52,6 +51,7 @@ while game_is_on:
         scoreboard.r_point()
         ball.reset_position()
 
+    # Reset ball position when it goes out of bounds
     if ball.xcor()>380:
         ball.reset_position()
 
